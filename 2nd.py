dd46ebc1c64522e630d529f55cd1723a039ff8cf
Kartikagrahari2003 <kartiktheking12345@gmail.com>
2026-05-02 15:55:15 +0530

    Marks Calculator

---
+++ 2nd.py
@@ -0,0 +1,6 @@
+print("\n---Marks Calculator---")
+x = float(input("Enter your Obtained Marks::: "))
+y = float(input("Enter total marks::: "))
+z = (x / y) * 100
+print(f"You got {x} out of {y}")
+print(f"Percentage= {z} %")
